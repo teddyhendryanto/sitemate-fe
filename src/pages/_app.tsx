82077d60dc44c6a1371551f942cca3fcce118965
Teddy Hendryanto <teddy.hendryanto@youtap.id>
2024-10-22 18:31:39 +0700

    feat: init and configure primereact

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
